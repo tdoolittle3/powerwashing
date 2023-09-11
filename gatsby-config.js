6c3190bab478e9f26c36df387a9bfb57fc93272a
Thomas Doolittle <tdoolittle3@gmail.com>
2023-09-10 18:45:26 -0600

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pensacola Pressurewashing`,
+    description: `We provide top-quality powerwashing services for your home or business.`,
+    author: `Doolittle`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+
 import Header from "./header"
 import "./layout.css"
 
@@ -26,6 +27,7 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
+
         style={{
           margin: `0 auto`,
           maxWidth: `var(--size-content)`,
